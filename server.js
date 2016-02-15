c7fad4f4363b116199b9de4796d622d98597859b
xobotun <xobotun@ya.ru>
2016-02-15 19:40:48 +0300

    Added some basic logging to express application.
    Also moved hardcoded '/public_html' to PUBLIC_DIR_NAME "constant".

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 var express = require('express'),
     errorHandler = require('errorhandler'),
-    app = express();
+    app = express(),
+	currentRequestNumber = 0;
 
 var HOSTNAME = 'localhost',
     PORT = 8080,
-    PUBLIC_DIR = __dirname + '/public_html';
+	PUBLIC_DIR_NAME = '/public_html',
+    PUBLIC_DIR = __dirname + PUBLIC_DIR_NAME;
 
-app.use(function (req) {
+app.use(function(req, res, next) {
+	var currentDate = new Date();
 	// Здесь нужно написать журналирование в формате
 	// (журналирование - вывод в консоль)
 	// [время] [номер запроса по счету]
+	console.log("-->%s %s %s\n   at %s:%s:%s", currentRequestNumber++, req.method, req.originalUrl, currentDate.getHours(), currentDate.getMinutes(), currentDate.getSeconds());
+	next();
 });
 
 app
 	.use('/', express.static(PUBLIC_DIR))
 	.use(errorHandler());
 
+
 app.listen(PORT, function () {
-	console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
+	console.log("-->Simple static server launched!\n   Running in %s\n   Listening to http://%s:%s", __dirname, HOSTNAME, PORT);
 });
