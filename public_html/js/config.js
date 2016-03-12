91be0e3ca520a9a149ede2fc4af8269a6a4bd882
Vitaly Nikolaev <vit3011@rambler.ru>
2016-03-12 18:06:23 +0300

    Three js first commit

--- public_html/js/config.js
+++ public_html/js/config.js
@@ -4,15 +4,24 @@ var require = {
     paths: {
         underscore: 'lib/underscore',
         jquery: 'lib/jquery',
-        backbone: 'lib/backbone'
+        backbone: 'lib/backbone',
+        three : 'lib/three.min'
     },
     shim: {
         'backbone': {
             deps: ['underscore', 'jquery'],
             exports: 'Backbone'
         },
+        'jquery': {
+          exports: 'jquery'
+        },
         'underscore': {
             exports: '_'
+        },
+
+        'three':{
+            exports: 'three'
         }
     }
+
 };
--- public_html/js/views/game.js
+++ public_html/js/views/game.js
@@ -1,10 +1,60 @@
 define(function (require) {
-        var baseView = require('views/baseView');
-        var tmpl =require('tmpl/game');
-        var View = baseView.extend({
-            template: tmpl
-        });
 
-        return new View();
+    var baseView = require('views/baseView');
+    var tmpl = require('tmpl/game');
+    var THREE = require('three');
+
+    var View = baseView.extend({
+        template: tmpl,
+        initialize: function () {
+            this.render();
+            var self = this;
+            var scene, camera, renderer;
+            var geometry, material, mesh;
+
+            init();
+            animate();
+
+            function init() {
+
+                scene = new THREE.Scene();
+
+                camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+                camera.position.z = 1000;
+
+                geometry = new THREE.BoxGeometry( 200, 200, 200 );
+                material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+                mesh = new THREE.Mesh( geometry, material );
+                scene.add( mesh );
+
+                renderer = new THREE.WebGLRenderer();
+                renderer.setSize( window.innerWidth, window.innerHeight );
+                var elem = self.$el;
+                elem.append( renderer.domElement );
+            }
+
+            function animate() {
+
+                requestAnimationFrame( animate );
+
+                mesh.rotation.x += 0.1;
+                mesh.rotation.y += 0.02;
+
+                renderer.render( scene, camera );
+
+            }
+        },
+        //show: function () {
+        //    $('#page').append(this.el);
+        //    $(this.el).show();
+        //},
+        //hide: function () {
+        //    $(this.el).remove();
+        //
+        //}
+
+    });
+    return new View();
     }
 );
