92e9ae52db340def30de8441444ce3bdc1c83dd1
Vitaly Nikolaev <vit3011@rambler.ru>
2016-04-15 01:29:26 +0300

    camera updated2

--- public_html/js/views/GameModules/gameInit.js
+++ public_html/js/views/GameModules/gameInit.js
@@ -116,7 +116,7 @@ define(function (require) {
             gameObjects.camera.updateProjectionMatrix();
         },
         setFocus: function (object) {
-            gameObjects.camera.position.set(object.position.x, object.position.y + 512, object.position.z - 512);
+            gameObjects.camera.position.set(object.position.x, object.position.y + 400, object.position.z - 950);
             gameObjects.camera.lookAt(object.position);
         },
         frame: function () {
