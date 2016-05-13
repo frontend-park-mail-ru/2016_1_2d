aca15d7b8b7e16f17351a7c4202714921f2a28f4
Vitaly Nikolaev <vit3011@rambler.ru>
2016-05-13 15:39:34 +0300

    simplify viewManager tests

--- public_html/js/views/viewManager.test.js
+++ public_html/js/views/viewManager.test.js
@@ -1,32 +1,27 @@
 define(function (require) {
-    // var ViewManager = require('views/viewManager');
-    // var LoginView = require('views/login');
-    // var RegView = require('views/register');
+    var ViewManager = require('views/viewManager');
+    var LoginView = require('views/login');
+    var RegView = require('views/register');
 
-    QUnit.ok();
 
-    // QUnit.module("views/manager");
-    //
-    // QUnit.test("ViewManager - экземпляр Backbone.View", function () {
-    //
-    //     var ViewManager = require('views/viewManager');
-    //     var viewManager = new ViewManager();
-    //
-    //     QUnit.ok(viewManager instanceof Backbone.View);
-    //
-    // });
+    QUnit.module("views/viewManager");
+    
+    QUnit.test("ViewManager - экземпляр Backbone.View", function () {
+        
+        var viewManager = new ViewManager();
+    
+        QUnit.ok(viewManager instanceof Backbone.View);
+    
+    });
 
-    // QUnit.test("Logic test for ViewManager", function () {
-    //
-    //     var views = {
-    //             login: LoginView,
-    //             reg: RegView
-    //         }; 
-    //     var viewManager = new ViewManager(views);
-    //
-    //     QUnit.equal(views['login'].$el.css('display') === 'none', true);
-    //     QUnit.equal(views['reg'].$el.css('display') === 'none', true);
-    //
-    // });
+    QUnit.test("Logic test for ViewManager", function () {
+        var views = {
+                login: LoginView,
+            }; 
+        var viewManager = new ViewManager(views);
+    
+        QUnit.equal(views['login'].$el.css('display') === 'none', true);
+    
+    });
 
 });
